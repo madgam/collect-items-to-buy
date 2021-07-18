46876e9ff402c637a91153eaeee3650c9758e8e4
武藤祐治 <madgam0731@gmail.com>
2021-07-18 16:06:32 +0900

    add: ルブタン用スクレイピング処理を追加

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+import csv
+import glob
+
 from modules.scrape import Scrape
 
 scrape = Scrape()
+csv_files = glob.glob('csv/in/*.csv')
+for csv_file in csv_files:
+    with open(csv_file, newline='', encoding="utf-8-sig") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        scrape.getData(spamreader)
--- src/modules/scrape.py
+++ src/modules/scrape.py
@@ -1,6 +1,40 @@
+import csv
+import datetime
+
+import requests
+from bs4 import BeautifulSoup
 
 
 class Scrape():
 
     def __init__(self):
-        print('I am Scrape.')
+        None
+
+    def getData(self, spamreader):
+        dt_now = datetime.datetime.now()
+        formated_dt_now = dt_now.strftime('%Y%m%d%H%M%S')
+        f = open('csv/out/out_{}.csv'.format(formated_dt_now), 'w')
+        writer = csv.writer(f)
+        data = []
+        COLUMNS = ['URL', '商品型番', '価格']
+        for c in COLUMNS:
+            data.append(c)
+        writer.writerow(data)
+        for row in spamreader:
+            uri = row[0]
+
+            data = []
+            data.append(uri)
+            res = requests.get(uri)
+            res.raise_for_status()
+            soup = BeautifulSoup(res.text, 'html.parser')
+            itemInfo = soup.select('.product-info-main')
+            itemTable = itemInfo[0].find(
+                class_='additional-attributes-wrapper')
+            itemDetail = itemTable.find_all('p')
+            data.append(itemDetail[2].text.split(':')[1].strip())
+            priceInfo = itemInfo[0].find(class_='product-info-price')
+            justPrice = priceInfo.find(class_='price')
+            data.append(justPrice.text)
+            writer.writerow(data)
+        f.close()
