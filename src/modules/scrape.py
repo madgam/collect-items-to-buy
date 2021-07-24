2a1d84bfbaedc88f77cd17b876c9f8423b324dd4
武藤祐治 <madgam0731@gmail.com>
2021-07-25 08:47:04 +0900

    modify: カウント処理を追加

--- src/modules/scrape.py
+++ src/modules/scrape.py
@@ -17,7 +17,7 @@ class Scrape():
         f = open('csv/out/out_{}.csv'.format(formated_dt_now), 'w')
         writer = csv.writer(f)
         data = []
-        COLUMNS = ['URL', '素材', 'カラー',
+        COLUMNS = ['URL', '商品名', '説明文', '素材', 'カラー',
                    '商品型番', 'コレクション', '価格', '最小サイズ', '最大サイズ']
         for c in COLUMNS:
             data.append(c)
@@ -26,43 +26,65 @@ class Scrape():
 
         async def process(uri):
             r = await assesion.get(uri)
-            await r.html.arender(wait=5, sleep=5)
+            await r.html.arender(wait=5, sleep=5, timeout=20)
             return r
 
-        for row in spamreader:
-            uri = row[0]
-            data = []
-            data.append(uri)
-            loop = asyncio.get_event_loop()
-            r = loop.run_until_complete(process(uri))
+        readerList = list(spamreader)
+        row_count = sum(1 for row in readerList)
 
-            # 商品データ
-            itemInfo = r.html.find('.product-info-main')
-            itemTable = itemInfo[0].find('.additional-attributes-wrapper')
+        count = 1
+        for row in readerList:
+            print('[INFO] データ取得処理中 ... {}/{}'.format(count, row_count))
+            try:
+                uri = row[0]
+                data = []
+                data.append(uri)
+                loop = asyncio.get_event_loop()
+                r = loop.run_until_complete(process(uri))
 
-            itemDetail = itemTable[0].find('p')
-            # 素材
-            data.append(itemDetail[0].text.split(':')[1].strip())
-            # カラー
-            data.append(itemDetail[1].text.split(':')[1].strip())
-            # 商品型番
-            data.append(itemDetail[2].text.split(':')[1].strip())
-            # コレクション
-            data.append(itemDetail[3].text.split(':')[1].strip())
+                # 商品名
+                productTitle = r.html.find('.breadcrumbs')[0].\
+                    find('.product')[0].text
+                data.append(productTitle)
+                # 商品データ
+                itemInfo = r.html.find('.product-info-main')
 
-            # 商品価格
-            priceInfo = itemInfo[0].find('.product-info-price')
-            justPrice = priceInfo[0].find('.price')
-            data.append(justPrice[0].text)
+                # 説明文
+                description = itemInfo[0].find('.overview')[0].text
+                data.append(description)
 
-            # サイズ一覧
-            itemSizes = itemInfo[0].find('.product-options-wrapper')
-            itemSizeList: List[str] = [
-                fr.text for fr in itemSizes[0].find('.in-stock')]
-            minimumSize = '-' if not itemSizeList else itemSizeList[0]
-            data.append(minimumSize)
-            maxSize = '-' if not itemSizeList else itemSizeList[-1]
-            data.append(maxSize)
-            writer.writerow(data)
+                itemTable = itemInfo[0].find('.additional-attributes-wrapper')
+                itemDetail = itemTable[0].find('p')
+                # 素材
+                data.append(itemDetail[0].text.split(':')[1].strip())
+                # カラー
+                data.append(itemDetail[1].text.split(':')[1].strip())
+                # 商品型番
+                data.append(itemDetail[2].text.split(':')[1].strip())
+                # コレクション
+                data.append(itemDetail[3].text.split(':')[1].strip())
+
+                # 商品価格
+                priceInfo = itemInfo[0].find('.product-info-price')
+                justPrice = priceInfo[0].find('.price')
+                data.append(justPrice[0].text)
+
+                # サイズ一覧
+                itemSizes = itemInfo[0].find('.product-options-wrapper')
+                itemSizeList: List[str] = [
+                    fr.text for fr in itemSizes[0].find('.super-attribute-select')[0].find(('li'))]
+                minimumSize = '-' if not itemSizeList else itemSizeList[0]
+                data.append(minimumSize)
+                maxSize = '-' if not itemSizeList else itemSizeList[-1]
+                data.append(maxSize)
+                writer.writerow(data)
+
+                # print('[ERROR] データ取得処理成功 ... {}'.format(count))
+                count += 1
+
+            except:
+                print('[ERROR] データ取得処理失敗 ... {}'.format(count))
+                count += 1
+                continue
 
         f.close()
