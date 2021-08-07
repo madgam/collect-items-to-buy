3ffbdc4b7443707fe92b881b39614a91b6088962
武藤祐治 <madgam0731@gmail.com>
2021-08-07 19:57:12 +0900

    add: CORS対応追加

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 from src.modules.scrape import Scrape
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 class Item(BaseModel):
