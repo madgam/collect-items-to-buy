22efa7388cb735ef8355da5ad316c49e0d15460f
武藤祐治 <madgam0731@gmail.com>
2021-08-08 22:52:44 +0900

    modify: CSVではなくJSONを返却するように修正

--- src/modules/scrape.py
+++ src/modules/scrape.py
@@ -1,11 +1,8 @@
 import asyncio
-import io
 from typing import List
 
 import aiohttp
-import pandas as pd
 from bs4 import BeautifulSoup
-from fastapi.responses import StreamingResponse
 
 
 class Scrape():
@@ -14,9 +11,8 @@ class Scrape():
         None
 
     async def getData(self, uriList: List[str]):
-        COLUMNS = ['URL', '商品名', '説明文', '素材', 'カラー',
-                   '商品型番', 'コレクション', '価格', '最小サイズ', '最大サイズ']
-        df = pd.DataFrame(columns=COLUMNS)
+        COLUMNS = ['url', 'item_name', 'description', 'material', 'color',
+                   'item_no', 'collection', 'price', 'size_minimum', 'size_max']
 
         async def aprocess(url, session):
             async with session.get(url) as response:
@@ -25,64 +21,57 @@ class Scrape():
                 return bs
 
         row_count = len(uriList)
+        jsonInnerData = []
 
         for i, uri in enumerate(uriList):
             print('[INFO] データ取得処理中 ... {}/{}'.format(i + 1, row_count))
             try:
-                data = []
-                data.append(uri)
+                data = {}
+                data[COLUMNS[0]] = uri
                 async with aiohttp.ClientSession() as session:
                     bs = await asyncio.gather(aprocess(uri, session))
 
                 # 商品名
                 productTitle = bs[0].find_all(class_='breadcrumbs')[0].\
                     select('.product')[0].text.strip()
-                data.append(productTitle)
+                data[COLUMNS[1]] = productTitle
                 # 商品データ
                 itemInfo = bs[0].find_all(class_='product-info-main')
 
                 # 説明文
                 description = itemInfo[0].select('.overview')[0].text.strip()
-                data.append(description)
+                data[COLUMNS[2]] = description
 
                 itemDetail = itemInfo[0].\
                     select('.additional-attributes-wrapper > p')
                 # 素材
-                data.append(itemDetail[0].text.split(':')[1].strip())
+                data[COLUMNS[3]] = itemDetail[0].text.split(':')[1].strip()
                 # カラー
-                data.append(itemDetail[1].text.split(':')[1].strip())
+                data[COLUMNS[4]] = itemDetail[1].text.split(':')[1].strip()
                 # 商品型番
-                data.append(itemDetail[2].text.split(':')[1].strip())
+                data[COLUMNS[5]] = itemDetail[2].text.split(':')[1].strip()
                 # コレクション
-                data.append(itemDetail[3].text.split(':')[1].strip())
+                data[COLUMNS[6]] = itemDetail[3].text.split(':')[1].strip()
 
                 # 商品価格
                 priceInfo = itemInfo[0].select('.product-info-price')
                 justPrice = priceInfo[0].select('.price')
-                data.append(justPrice[0].text.strip())
+                data[COLUMNS[7]] = justPrice[0].text.strip()
 
                 # サイズ一覧
                 itemSizes = itemInfo[0].select('.product-options-wrapper')
                 itemSizeList: List[str] = [
                     fr.text.strip() for fr in itemSizes[0].select('.super-attribute-select > li')]
                 minimumSize = '-' if not itemSizeList else itemSizeList[0]
-                data.append(minimumSize)
+                data[COLUMNS[8]] = minimumSize
                 maxSize = '-' if not itemSizeList else itemSizeList[-1]
-                data.append(maxSize)
+                data[COLUMNS[9]] = maxSize
 
-                df.loc[i] = data
+                jsonInnerData.append(data)
                 # print('[ERROR] データ取得処理成功 ... {}'.format(count))
 
             except Exception as e:
                 print('[ERROR] データ取得処理失敗 ... {}:[{}]'.format(i + 1, e))
                 continue
 
-        stream = io.StringIO()
-        df.to_csv(stream, index=False)
-        # stream.getvalue()によってデータを取り出し
-        response = StreamingResponse(
-            iter([stream.getvalue()]), media_type="text/csv")
-
-        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
-
-        return response
+        return jsonInnerData
